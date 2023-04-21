4c9bd2712b15c5860ccac8483f063183d2338f04
Douglas Ramírez <ra.douglas@hotmail.com>
2023-04-21 11:51:30 -0500

    Update login.vue

--- pages/ds/login.vue
+++ pages/ds/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="grid flex-column justify-content-center min-w-screen">
+  <section class="grid flex-column justify-content-center min-h-screen">
     <div class="col-12 md:col-6 md:col-offset-3">
       <Card>
         <template #title>Sign in</template>
@@ -7,11 +7,11 @@
           <form @submit.prevent="submit">
             <div class="p-inputgroup flex-1 mb-2">
               <span class="p-inputgroup-addon"><i class="pi pi-at"></i></span>
-              <InputText v-model="credentials.email" type="email" required/>
+              <InputText name="email" v-model="credentials.email" type="email" required placeholder="example@example.com"/>
             </div>
             <div class="p-inputgroup flex-1 mb-2">
               <span class="p-inputgroup-addon"><i class="pi pi-lock"></i></span>
-              <InputText v-model="credentials.password" type="password" required />
+              <InputText v-model="credentials.password" type="password" required placeholder="password"/>
             </div>
             <Button type="submit">Submit</Button>
           </form>
