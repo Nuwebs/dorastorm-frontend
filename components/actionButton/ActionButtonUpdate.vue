a329ceaaad6e614b804a9a624eb66d04cebf70ca
Douglas Ramírez <ra.douglas@hotmail.com>
2023-05-03 12:30:07 -0500

    Create ActionButtonUpdate.vue

---
+++ components/actionButton/ActionButtonUpdate.vue
@@ -0,0 +1,30 @@
+<template>
+  <Button icon="pi pi-pencil" text rounded aria-label="Update" @click="push" />
+</template>
+
+<script setup lang="ts">
+import Button from 'primevue/button';
+import { useRouter, useLocalePath } from '#imports';
+
+interface Props {
+  route: string;
+  modelId: number | string;
+  modelIdReplace?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelIdReplace: "{id}",
+});
+
+if (!props.route.includes(props.modelIdReplace)) {
+  console.error(`The route ${props.route} does not includes the replace key ${props.modelIdReplace}`);
+}
+
+const router = useRouter();
+const lp = useLocalePath();
+
+function push(): void {
+  const r = props.route.replace(props.modelIdReplace, String(props.modelId));
+  router.push(lp(r));
+}
+</script>
