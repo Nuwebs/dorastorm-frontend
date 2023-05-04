47b3d59fa56752d9cd93418906f6bfdcf3378122
Douglas Ramírez <ra.douglas@hotmail.com>
2023-05-04 11:05:47 -0500

    Basic 404 for DS related sites

---
+++ components/TheDS404.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="p-container py-8 grid grid-nogutter">
+    <div class="col-4 flex justify-content-end">
+      <i class="pi pi-map text-8xl mr-4"></i>
+    </div>
+    <div class="col-8 flex flex-column justify-content-center">
+      <h1 class="m-0 text-2xl">{{ props.title? title :$t("error.404.default_title") }}</h1>
+      <h3 class="m-0 text-color-secondary">{{ props.subtitle? subtitle : $t("error.404.default_resource") }}</h3>
+    </div>
+  </section>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string;
+  subtitle?: string;
+}>();
+</script>
