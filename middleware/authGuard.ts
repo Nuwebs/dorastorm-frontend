55a6b27d7b554dea30d8a4b7978e4c8cc801bdba
Douglas Ramírez <ra.douglas@hotmail.com>
2023-05-08 11:50:20 -0500

    Layout bugfix

--- middleware/authGuard.ts
+++ middleware/authGuard.ts
@@ -2,10 +2,10 @@ import { defineNuxtRouteMiddleware, navigateTo, setPageLayout } from "nuxt/app";
 import useAuthStore from "~/stores/authStore";
 import { DsRouteMeta } from "~/types/dorastorm";
 export default defineNuxtRouteMiddleware(
-  (to: DsRouteMeta, from: DsRouteMeta) => {
-    if (!to.meta.layout) setPageLayout("ds");    
+  (to: DsRouteMeta, from: DsRouteMeta) => {    
     const authStore = useAuthStore();
     if (!authStore.isLoggedIn) return navigateTo("/login");
+    if (!to.meta.layout) setPageLayout("ds");
     if (!to.meta.permissions) return;
     const checker = to.meta.strictPermissions
       ? authStore.hasEveryPermissions
