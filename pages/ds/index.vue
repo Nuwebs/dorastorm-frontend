e4a08da3a9e40a72fa430d032eeea599a31cec6f
Douglas Ramírez <ra.douglas@hotmail.com>
2023-05-15 10:59:23 -0500

    Internal home page

--- pages/ds/index.vue
+++ pages/ds/index.vue
@@ -1,67 +1,33 @@
 <template>
-  <section>
-    <h1>Home page for logged users</h1>
-    <DataTableBase :data="data" :global-filter-fields="['nombre', 'apellido']" v-model:filters="filters" expandable
-      lazy-paginator @filter="test" :total-records="data.length">
-      <template #globalFilter>
-        <input type="text" v-model="filters.global.value">
+  <section class="p-container">
+    <Card class="shadow-1 mb-3" v-if="authStore.user">
+      <template #title>
+        {{ $t("general.welcome") }}
       </template>
-      <Column field="nombre" header="Nombre" sortable>
-        <template #filter="{ filterModel, filterCallback }">
-          <input type="text" class="p-column-filter" v-model="filterModel.value" @keydown.enter="filterCallback()">
-        </template>
-      </Column>
-      <Column field="apellido" header="Apellido" sortable />
-      <template #expansion="test: any">
-        <h1>{{ test.data.nombre }}</h1>
+      <template #content>
+        <UserDataRow :user="authStore.user"></UserDataRow>
       </template>
-    </DataTableBase>
+    </Card>
+    <Card class="shadow-1 h-15rem">
+      <template #title>
+        Custom card
+      </template>
+      <template #content>
+        <p>Your custom information system content!</p>
+      </template>
+    </Card>
   </section>
 </template>
 
 <script setup lang="ts">
 import { definePageMeta } from '#imports';
-import { FilterMatchMode } from 'primevue/api';
-import { ref } from "vue";
-import Column from "primevue/column";
-import DataTableBase from '~/components/dataTable/DataTableBase.vue';
+import Card from 'primevue/card';
+import UserDataRow from '~/components/user/UserDataRow.vue';
+import useAuthStore from '~/stores/authStore';
 
-// This page shouldn't have any permission due to be the base logged in
-// users page.
 definePageMeta({
-  middleware: ['auth-guard']
+  middleware: ["auth-guard"]
 });
 
-const data = ref([
-  {
-    nombre: "Douglas",
-    apellido: "Ramírez"
-  },
-  {
-    nombre: "Zemprom",
-    apellido: "Andrés"
-  }
-]);
-
-const filters = ref({
-  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-  nombre: { value: null, matchMode: FilterMatchMode.CONTAINS }
-});
-
-const columns = [
-  {
-    fieldName: "nombre",
-    header: "Nombre",
-    sortable: true
-  },
-  {
-    fieldName: "apellido",
-    header: "Apellido",
-    sortable: true
-  }
-];
-
-function test(param: any) {
-  console.log(param);
-}
+const authStore = useAuthStore();
 </script>
