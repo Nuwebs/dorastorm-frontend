c2c5751e2a808a7364cac50284f71b36f6f60159
Douglas Ramírez <ra.douglas@hotmail.com>
2023-05-15 11:18:44 -0500

    Home page button

--- components/TheDSNavbar.vue
+++ components/TheDSNavbar.vue
@@ -4,6 +4,7 @@
       <div class="flex align-items-center mr-2">
         <h3 class="my-0 mr-2">Dorastorm</h3>
         <NavbarSidebarButton @click="$emit('sidebarButtonClick')"/>
+        <NavbarHomeButton @click="router.push('/ds')"/>
       </div>
     </template>
     <template #end>
@@ -16,4 +17,8 @@
 import NavbarBase from "./navbar/NavbarBase.vue";
 import NavbarSignoutButton from "./navbar/NavbarSignoutButton.vue";
 import NavbarSidebarButton from "./navbar/NavbarSidebarButton.vue";
+import NavbarHomeButton from "./navbar/NavbarHomeButton.vue";
+import { useRouter } from "#imports";
+
+const router = useRouter();
 </script>
---
+++ components/navbar/NavbarHomeButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <Button 
+    text
+    plain
+    icon="pi pi-home"
+    rounded
+    aria-label="Home"
+    class="m-center"
+  />
+</template>
+
+<script setup lang="ts">
+import Button from "primevue/button";
+</script>
+<style scoped>
+.m-center {
+  top: 2px
+}
+</style>
