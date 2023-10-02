31968e545cc2ee7d38867b5a8b3453dc0efec285
Douglas Ramírez <ra.douglas@hotmail.com>
2023-10-02 11:03:13 -0500

    Styles overhaul completed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,8 @@ import {
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: [
-    "primevue/resources/themes/lara-light-blue/theme.css",
+    "/assets/styles/ds.css",
+    "/assets/styles/fonts.css",
     "primevue/resources/primevue.css",
     "primeflex/primeflex.css",
     "primeicons/primeicons.css",
