c5a1348e867d4f253fe718f8b368f0766af4be06
Douglas Ramírez <ra.douglas@hotmail.com>
2023-11-14 17:21:04 -0500

    Transparent navbar bugfix

--- components/navbar/NavbarBase.vue
+++ components/navbar/NavbarBase.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="shadow-1 navbar">
-    <Menubar class="py-3 px-6 h-full" :model="content">
+    <Menubar class="py-3 px-6 h-full surface-section" :model="content">
       <template #start>
         <div class="flex align-items-center">
           <h3 class="my-0 mr-4">Dorastorm</h3>
