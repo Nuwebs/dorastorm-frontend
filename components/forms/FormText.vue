533be4454e921d1a789b339741d988132d8c472a
Douglas <ra.douglas@hotmail.com>
2024-12-21 22:39:50 -0500

    feat: Zod + VeeValidate installed + FormText

---
+++ components/forms/FormText.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { useField, ErrorMessage } from 'vee-validate';
+import { InputText } from 'primevue';
+
+const props = defineProps<{
+  name: string;
+  label: string;
+  modelValue?: string | null;
+  type: 'text' | 'password' | 'email';
+}>();
+
+const { value, errorMessage } = useField<typeof props.modelValue>(
+  props.name,
+  undefined,
+  {
+    syncVModel: true
+  }
+);
+</script>
+
+<template>
+  <div>
+    <slot name="label">
+      <label :for="name" class="block">{{ label }}</label>
+    </slot>
+    <InputText
+      v-model="value"
+      :title="name"
+      class="w-full"
+      :type="type"
+      :invalid="errorMessage !== undefined"
+    />
+    <slot name="errorMessage" :error-message-bag="errorMessage">
+      <ErrorMessage :name="name" class="text-red-400" />
+    </slot>
+  </div>
+</template>
+
+<style scoped></style>
