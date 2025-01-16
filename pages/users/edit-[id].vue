8db6d9e90136548d2a7656bf6437d9196567e65f
Douglas <ra.douglas@hotmail.com>
2025-01-16 17:45:48 -0500

    refactor: User edit now uses Nuxt await capabilities

--- pages/users/edit-[id].vue
+++ pages/users/edit-[id].vue
@@ -1,42 +1,37 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { definePageMeta, useRoute } from '#imports';
+import { createError, definePageMeta, useI18n, useRoute } from '#imports';
 import { UserService } from '~/services/user-service';
 import UserFormContainer from '~/components/user/form/UserFormContainer.vue';
-import type { UpdateUser, User } from '~/types/user';
+import type { UpdateUser } from '~/types/user';
 
 definePageMeta({
   middleware: ['auth-guard']
 });
 
+const { t } = useI18n();
 const route = useRoute();
-const loading = ref<boolean>(false);
-const user = ref<User | null>(null);
 
 const userService = new UserService();
+
+const { data, error } = await userService.findById(Number(route.params.id));
+
+if (error || data === null) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: t('error.404.specific.user'),
+    fatal: true
+  });
+}
 
 async function submitHandler(payload: UpdateUser) {
   return userService.update(Number(route.params.id), payload);
 }
-
-onMounted(async () => {
-  loading.value = true;
-  const { data, error } = await userService.findById(Number(route.params.id));
-  loading.value = false;
-
-  if (error) return;
-
-  user.value = data;
-});
 </script>
 
 <template>
-  <p v-if="loading">Cargando...</p>
-  <div v-else-if="!loading && !user">404</div>
   <UserFormContainer
-    v-else
     mode="update"
-    :initial-data="user!"
+    :initial-data="data"
     :submit-handler="submitHandler"
   />
 </template>
