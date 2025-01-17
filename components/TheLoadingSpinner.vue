c5922200fd72b11fc67567c297ff7bde3dcdbb95
Douglas <ra.douglas@hotmail.com>
2025-01-17 18:27:42 -0500

    feat: Basic loading spinner

---
+++ components/TheLoadingSpinner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex items-center justify-center">
+    <div role="status" aria-label="Loading" :class="spinnerClass" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+interface SpinnerProps {
+  size?: string;
+}
+
+const props = defineProps<SpinnerProps>();
+const { size = '20' } = props;
+
+const spinnerClass = computed(() => {
+  return `w-${size} h-${size} border-4 border-primary-300 rounded-full animate-spin`;
+});
+</script>
