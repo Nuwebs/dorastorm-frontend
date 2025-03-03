e4be84fa90830beb2fb6d6bc14f7e387b65f3387
Douglas <ra.douglas@hotmail.com>
2025-03-03 17:20:10 -0500

    chore: Shadcn Button added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
   css: ['~/assets/styles/ds.css', 'primeicons/primeicons.css'],
 
   shadcn: {
-    prefix: '',
+    prefix: 'Ui',
     componentDir: './components/ui'
   },
 
