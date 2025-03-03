ae55947a1623d8371ddbaa2fc5e8999d052e80ec
Douglas <ra.douglas@hotmail.com>
2025-03-03 18:46:42 -0500

    feat: UiCard components family

---
+++ components/ui/card/index.ts
@@ -0,0 +1,6 @@
+export { default as UiCard } from './UiCard.vue';
+export { default as UiCardContent } from './UiCardContent.vue';
+export { default as UiCardDescription } from './UiCardDescription.vue';
+export { default as UiCardFooter } from './UiCardFooter.vue';
+export { default as UiCardHeader } from './UiCardHeader.vue';
+export { default as UiCardTitle } from './UiCardTitle.vue';
