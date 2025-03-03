b35ea60467005e6e17ccd3831f9655ddfef09380
Douglas <ra.douglas@hotmail.com>
2025-03-03 17:17:05 -0500

    chore: App.vue cleaned

--- app.vue
+++ app.vue
@@ -1,33 +1,10 @@
-<script setup lang="ts">
-import { Toast, useToast, ConfirmDialog } from 'primevue';
-import { onMounted, watch } from 'vue';
-import useExternalToastStore from './stores/external-toast-store';
-import { useI18n } from '#imports';
-
-const toastStore = useExternalToastStore();
-const toast = useToast();
-const { t } = useI18n();
-
-function renderComposablesToast() {
-  toastStore.processToasts(toast.add, t);
-}
-
-onMounted(() => {
-  renderComposablesToast();
-});
-
-watch(toastStore._toasts, () => {
-  renderComposablesToast();
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <NuxtLoadingIndicator />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-  <Toast />
-  <ConfirmDialog />
 </template>
 
 <style>
