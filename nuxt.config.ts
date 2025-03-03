bf8ba38e399996f77372773c9bcf24ebb1260e2c
Douglas <ra.douglas@hotmail.com>
2025-03-03 16:21:39 -0500

    chore: Nuxt native Tailwind installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,13 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', 'nuxt-zod-i18n', '@nuxtjs/i18n', '@nuxt/eslint'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    'nuxt-zod-i18n',
+    '@nuxtjs/i18n',
+    '@nuxt/eslint'
+  ],
   css: ['~/assets/styles/ds.css', 'primeicons/primeicons.css'],
 
   i18n: {
