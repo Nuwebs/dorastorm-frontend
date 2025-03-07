d8b4895f0d24e6ee80893684569791bb72fb2b89
Douglas <ra.douglas@hotmail.com>
2025-03-07 15:32:17 -0500

    feat(Ui): Pagination components

---
+++ components/ui/pagination/UiPaginationLast.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ChevronsRight } from 'lucide-vue-next';
+import { PaginationLast, type PaginationLastProps } from 'reka-ui';
+import { computed, type HTMLAttributes } from 'vue';
+import UiButton from '../button/UiButton.vue';
+import { cn } from '~/lib/utils';
+
+const props = withDefaults(
+  defineProps<PaginationLastProps & { class?: HTMLAttributes['class'] }>(),
+  {
+    asChild: true
+  }
+);
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+</script>
+
+<template>
+  <PaginationLast v-bind="delegatedProps">
+    <UiButton :class="cn('w-10 h-10 p-0', props.class)" variant="outline">
+      <slot>
+        <ChevronsRight class="h-4 w-4" />
+      </slot>
+    </UiButton>
+  </PaginationLast>
+</template>
