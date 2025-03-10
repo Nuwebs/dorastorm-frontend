1d6e3e780b5fbbb35901c8666d017cad5c9b9528
Douglas <ra.douglas@hotmail.com>
2025-03-10 14:56:47 -0500

    refactor: User and Roles modules indexes refactored into the lazy loading template

--- pages/roles/index.vue
+++ pages/roles/index.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
 import type { ColumnDef } from '@tanstack/vue-table';
+import { useTemplateRef } from 'vue';
 import { definePageMeta, useI18n } from '#imports';
-import ButtonActionRefresh from '~/components/button/action/ButtonActionRefresh.vue';
-import ButtonActionSearch from '~/components/button/action/ButtonActionSearch.vue';
 import ButtonActionUpdate from '~/components/button/action/ButtonActionUpdate.vue';
-import DataTable from '~/components/datatable/DataTable.vue';
 import RoleDataRow from '~/components/role/RoleDataRow.vue';
 import RoleDeleteButton from '~/components/role/RoleDeleteButton.vue';
-import TheLoadingSpinner from '~/components/TheLoadingSpinner.vue';
-import UiInput from '~/components/ui/input/UiInput.vue';
+import TemplateLazyLoadingIndex from '~/components/template/TemplateLazyLoadingIndex.vue';
 import useCachedPermissions from '~/composables/useCachedPermissions';
-import useLaravelLazyPagination from '~/composables/useLaravelLazyPagination';
 import { PERMISSION } from '~/services/permission-service';
 import { RoleService } from '~/services/role-service';
 import type { Role } from '~/types/role';
@@ -26,25 +22,9 @@ const { userCan, roleCan, userIsAllowed } = useCachedPermissions([
   PERMISSION.ROLES_UPDATE
 ]);
 const service = new RoleService();
-const {
-  paginationData,
-  filters,
-  loading,
-  totalResults,
-  resultsPerPage,
-  currentPage,
-  search,
-  calculatePageAfterNItemsDeletion
-} = useLaravelLazyPagination<Role>(service, {
-  global: null
-});
 
 const columns: ColumnDef<Role>[] = [
-  {
-    accessorKey: 'id',
-    header: t('general.id'),
-    enableSorting: false
-  },
+  { accessorKey: 'id', header: t('general.id'), enableSorting: false },
   {
     accessorKey: 'name',
     header: t('modules.roles.name'),
@@ -55,82 +35,41 @@ const columns: ColumnDef<Role>[] = [
     header: t('modules.roles.description'),
     enableSorting: false
   },
-  {
-    accessorKey: 'action',
-    header: t('general.action'),
-    enableSorting: false
-  }
+  { accessorKey: 'action', header: t('general.action'), enableSorting: false }
 ];
 
-async function handleRoleDeleted(): Promise<void> {
-  search(calculatePageAfterNItemsDeletion());
-}
+const templateRef = useTemplateRef('index');
+const handleRoleDeleted = () => {
+  templateRef.value?.refresh();
+};
 </script>
+
 <template>
-  <section class="container">
-    <h1>
-      {{ $t('modules.roles.list') }}
-    </h1>
-
-    <DataTable
-      :columns="columns"
-      :data="paginationData?.data ?? []"
-      :loading="loading"
-      :total-records="totalResults"
-      :rows-per-page="resultsPerPage"
-      lazy
-      paginatable
-      expandable
-      @update:pagination="(e) => search(e.pageIndex + 1)"
-    >
-      <template #table-header="{ table }">
-        <div class="flex justify-between">
-          <ButtonActionRefresh
-            :loading="loading"
-            @click="search(currentPage)"
-          />
-          <div>
-            <div class="flex">
-              <UiInput
-                v-model="filters['global']"
-                type="text"
-                @update:model-value="table.setGlobalFilter($event)"
-                @keyup.enter="search()"
-              />
-              <ButtonActionSearch
-                class="rounded-l-none"
-                :loading="loading"
-                @click="search()"
-              />
-            </div>
-          </div>
-        </div>
-      </template>
-
-      <template #loading>
-        <TheLoadingSpinner />
-      </template>
-
-      <template #expanded="row">
-        <RoleDataRow :role="row.row.original" />
-      </template>
-
-      <template v-if="userIsAllowed" #cell-action="{ rowValue: data }">
-        <RoleDeleteButton
-          v-if="
-            userCan(PERMISSION.ROLES_DELETE) && roleCan(data.hierarchy, false)
-          "
-          :role="data"
-          @deleted="handleRoleDeleted"
-        />
-        <ButtonActionUpdate
-          v-if="
-            userCan(PERMISSION.ROLES_UPDATE) && roleCan(data.hierarchy, false)
-          "
-          route="/roles/edit-{id}"
-          :model-id="data.id"
-        />
-      </template>
-    </DataTable>
-  </section>
+  <TemplateLazyLoadingIndex
+    ref="index"
+    :service="service"
+    :columns="columns"
+    module-name="roles"
+    has-expandable
+  >
+    <template #expanded="{ row }">
+      <RoleDataRow :role="row.original" />
+    </template>
+    <template v-if="userIsAllowed" #action="{ rowValue: data }">
+      <RoleDeleteButton
+        v-if="
+          userCan(PERMISSION.ROLES_DELETE) && roleCan(data.hierarchy, false)
+        "
+        :role="data"
+        @deleted="handleRoleDeleted"
+      />
+      <ButtonActionUpdate
+        v-if="
+          userCan(PERMISSION.ROLES_UPDATE) && roleCan(data.hierarchy, false)
+        "
+        route="/roles/edit-{id}"
+        :model-id="data.id"
+      />
+    </template>
+  </TemplateLazyLoadingIndex>
 </template>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
 import type { ColumnDef } from '@tanstack/vue-table';
+import { useTemplateRef } from 'vue';
 import { definePageMeta, useI18n } from '#imports';
-import ButtonActionRefresh from '~/components/button/action/ButtonActionRefresh.vue';
-import ButtonActionSearch from '~/components/button/action/ButtonActionSearch.vue';
 import ButtonActionUpdate from '~/components/button/action/ButtonActionUpdate.vue';
-import DataTable from '~/components/datatable/DataTable.vue';
-import TheLoadingSpinner from '~/components/TheLoadingSpinner.vue';
-import UiInput from '~/components/ui/input/UiInput.vue';
+import TemplateLazyLoadingIndex from '~/components/template/TemplateLazyLoadingIndex.vue';
 import UserDeleteButton from '~/components/user/UserDeleteButton.vue';
 import useCachedPermissions from '~/composables/useCachedPermissions';
-import useLaravelLazyPagination from '~/composables/useLaravelLazyPagination';
 import { PERMISSION } from '~/services/permission-service';
 import { UserService } from '~/services/user-service';
 import type { User } from '~/types/user';
@@ -24,27 +20,10 @@ const { userCan, roleCan, userIsAllowed } = useCachedPermissions([
   PERMISSION.USERS_UPDATE,
   PERMISSION.USERS_DELETE
 ]);
-
 const service = new UserService();
-const {
-  paginationData,
-  filters,
-  loading,
-  totalResults,
-  resultsPerPage,
-  currentPage,
-  search,
-  calculatePageAfterNItemsDeletion
-} = useLaravelLazyPagination<User>(service, {
-  global: null
-});
 
 const columns: ColumnDef<User>[] = [
-  {
-    accessorKey: 'id',
-    header: t('general.id'),
-    enableSorting: false
-  },
+  { accessorKey: 'id', header: t('general.id'), enableSorting: false },
   {
     accessorKey: 'name',
     header: t('modules.users.name'),
@@ -55,82 +34,38 @@ const columns: ColumnDef<User>[] = [
     header: t('modules.users.role'),
     enableSorting: false
   },
-  {
-    accessorKey: 'email',
-    header: t('forms.email'),
-    enableSorting: false
-  },
-  {
-    accessorKey: 'action',
-    header: t('general.action'),
-    enableSorting: false
-  }
+  { accessorKey: 'email', header: t('forms.email'), enableSorting: false },
+  { accessorKey: 'action', header: t('general.action'), enableSorting: false }
 ];
 
-async function handleUserDeleted(): Promise<void> {
-  search(calculatePageAfterNItemsDeletion());
-}
+const templateRef = useTemplateRef('index');
+const handleUserDeleted = () => {
+  templateRef.value?.refresh();
+};
 </script>
+
 <template>
-  <section class="container">
-    <h1>{{ $t('modules.users.list') }}</h1>
-    <DataTable
-      :columns="columns"
-      :data="paginationData?.data ?? []"
-      :loading="loading"
-      :total-records="totalResults"
-      :rows-per-page="resultsPerPage"
-      lazy
-      paginatable
-      @update:pagination="(e) => search(e.pageIndex + 1)"
-    >
-      <template #table-header="{ table }">
-        <div class="flex justify-between">
-          <ButtonActionRefresh
-            :loading="loading"
-            @click="search(currentPage)"
-          />
-          <div>
-            <div class="flex">
-              <UiInput
-                v-model="filters['global']"
-                type="text"
-                class="rounded-r-none"
-                @update:model-value="table.setGlobalFilter($event)"
-                @keyup.enter="search()"
-              />
-              <ButtonActionSearch
-                class="rounded-l-none"
-                :loading="loading"
-                @click="search()"
-              />
-            </div>
-          </div>
-        </div>
-      </template>
-
-      <template #loading>
-        <TheLoadingSpinner />
-      </template>
-
-      <template v-if="userIsAllowed" #cell-action="{ rowValue: data }">
-        <UserDeleteButton
-          v-if="
-            userCan(PERMISSION.USERS_UPDATE) &&
-            roleCan(data.role.hierarchy, true)
-          "
-          :user="data"
-          @deleted="handleUserDeleted"
-        />
-        <ButtonActionUpdate
-          v-if="
-            userCan(PERMISSION.USERS_UPDATE) &&
-            roleCan(data.role.hierarchy, true)
-          "
-          route="/users/edit-{id}"
-          :model-id="data.id"
-        />
-      </template>
-    </DataTable>
-  </section>
+  <TemplateLazyLoadingIndex
+    ref="index"
+    :service="service"
+    :columns="columns"
+    module-name="users"
+  >
+    <template v-if="userIsAllowed" #action="{ rowValue: data }">
+      <UserDeleteButton
+        v-if="
+          userCan(PERMISSION.USERS_UPDATE) && roleCan(data.role.hierarchy, true)
+        "
+        :user="data"
+        @deleted="handleUserDeleted"
+      />
+      <ButtonActionUpdate
+        v-if="
+          userCan(PERMISSION.USERS_UPDATE) && roleCan(data.role.hierarchy, true)
+        "
+        route="/users/edit-{id}"
+        :model-id="data.id"
+      />
+    </template>
+  </TemplateLazyLoadingIndex>
 </template>
